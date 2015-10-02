3ef7afba4aae6f182f44d662dab38139d4c66ae6
Anderson Arboleya <anderson@arboleya.me>
2015-10-02 14:28:23 -0300

    fixing target

--- make.js
+++ make.js
@@ -220,7 +220,7 @@ target['deps.upgrade'] = function(){
 
 target.publish = function(){
   var version = require('./package.json').version;
-  shell.exec('git tag -a '+ version +' -m "Releasing '+ version +'")');
+  shell.exec('git tag -a '+ version +' -m "Releasing '+ version +'"');
   shell.exec('git push origin master --tags');
   shell.exec('npm publish');
   shell.exec('meteor publish');
