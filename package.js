5df3dd9b565941d88d3c3ebe7e876120a9d678ef
Anderson Arboleya <anderson@arboleya.me>
2015-10-02 12:11:46 -0300

    reusing version number from package.json

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'arboleya:electrify',
-  version: '1.3.0',
+  version: require('./package.json').version,
   summary: 'Package your Meteor apps with Electron, and butter.',
   git: 'https://github.com/arboleya/electrify',
   documentation: 'README.md'
